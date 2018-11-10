b40bff6ce645563ac175248ec02bb67f616fb480
Toby Kreiman <tobykreiman@gmail.com>
2018-11-10 15:35:30 -0500

    Update __init__.py

--- Average/check50/__init__.py
+++ Average/check50/__init__.py
@@ -12,7 +12,7 @@ class Average(Checks):
         """Average.c compiles"""
         self.spawn("clang -o Average Average.c -lcs50 -lm").exit(0)
 
-    @check("Averages 2 Numbers")
+    @check("compiles")
     def test_1_10_input(self):
         """Outputs 5.5"""
         self.spawn("./Average 1 10").stdout("5.5\n", "5.5\n").exit(0)
